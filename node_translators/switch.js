4f5f73732ea7df4f18e0d1bb90d63308716bc11e
Christopher Luna <dev@christopher-luna.com>
2016-11-14 22:41:34 -0600

    Indenting switch like psr-2 says

--- node_translators/switch.js
+++ node_translators/switch.js
@@ -10,11 +10,11 @@ module.exports = function (node, indent) {
   cases = node[2].map(function (cas) {
     var head;
     if (cas.condition) {
-      head = indent + 'case ' + codegen(cas.condition, indent) + ':' + that.nl;
+      head = indent + indent + 'case ' + codegen(cas.condition, indent) + ':' + that.nl;
     } else {
-      head = indent + 'default:' + that.nl;
+      head = indent + indent + 'default:' + that.nl;
     }
-    return head + body(codegen, indent, that.indent, that.nl, cas.body);
+    return head + body(codegen, indent + indent, that.indent, that.nl, cas.body);
   });
   str += cases.join('') + indent + '}';
   return str;
