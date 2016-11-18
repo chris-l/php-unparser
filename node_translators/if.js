225bf8a494b9dba290c209c539346bf6462dc896
Christopher Luna <dev@christopher-luna.com>
2016-11-17 18:09:02 -0600

    Correct "elseif" case

--- node_translators/if.js
+++ node_translators/if.js
@@ -8,12 +8,16 @@ module.exports = function processIf(node, indent) {
   codegen = this.process.bind(this);
   str = 'if' + this.ws + '(' + codegen(node[1], indent) + ')' + this.ws + '{' + this.nl +
     body(codegen, indent, this.indent, this.nl, node[2]) + indent + '}';
-  if (node[3] && node[3][0] === 'if') {
-    str += this.ws + 'else' + this.ws + processIf.call(this, node[3], indent);
+
+  // is an "elseif"
+  if (node[3] && node[3][0][0] === 'if') {
+    str += this.ws + 'else' + processIf.call(this, node[3][0], indent);
   }
 
-  if (node[3] && node[3][0] !== 'if') {
-    str += this.ws + 'else' + this.ws + '{' + this.nl + body(codegen, indent, this.indent, this.nl, node[3]) + indent + '}' + this.nl;
+  // is an "else"
+  if (node[3] && node[3][0][0] !== 'if') {
+    str += this.ws + 'else' + this.ws + '{' + this.nl;
+    str += body(codegen, indent, this.indent, this.nl, node[3]) + indent + '}' + this.nl;
   }
   return str;
 };
