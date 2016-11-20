9a3545ea3f52d657a3c518ae9ea06887fdd9c4b4
Christopher Luna <dev@christopher-luna.com>
2016-11-20 02:31:27 -0600

    Allow to ask the raw string

--- node_translators/string.js
+++ node_translators/string.js
@@ -1,7 +1,11 @@
-/*jslint node: true, indent: 2 */
+/*jslint node: true, indent: 2, unparam:true */
 'use strict';
 
-module.exports = function (node) {
+module.exports = function (node, indent, opt) {
+  opt = opt || {};
+  if (opt.raw) {
+    return node[1];
+  }
   return JSON.stringify(node[1]);
 };
 
