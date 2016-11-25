e2220e158d2aee8d8a538e85dc767fe20c882b49
Christopher Luna <dev@christopher-luna.com>
2016-11-24 23:42:09 -0600

    First page commit

---
+++ demo.js
@@ -0,0 +1,26 @@
+/* This program is released under the MIT license http://opensource.org/licenses/MIT */
+/*jslint indent: 2, regexp:true*/
+/*global phpUnparser, phpParser, document*/
+
+(function () {
+  'use strict';
+  var codeEle = document.querySelector('#code');
+
+  function changeCode() {
+    var code, output;
+
+    code = codeEle.value;
+    output = document.querySelector('#output');
+    try {
+      output.textContent = phpUnparser(phpParser.parseEval(code));
+    } catch (e) {
+      output.textContent = '***Error***\nThe entered string seems to be invalid php';
+    }
+  }
+
+  codeEle.addEventListener('change', changeCode);
+  codeEle.addEventListener('keyup', changeCode);
+  setTimeout(function () {
+    changeCode();
+  }, 1000);
+}());
