e12c9071184d35e9468d1ae99366b82e5907fc4f
Ioan CHIRIAC <i.chiriac@yahoo.com>
2016-12-14 01:26:16 +0100

    add interfaces

---
+++ node_translators/helper/keywords.js
@@ -0,0 +1,27 @@
+/*jslint node: true, indent: 2 */
+
+module.exports = function addKeywords(x) {
+  var out = '';
+
+  if (x[2] === 1) {
+    out += 'abstract ';
+  }
+  if (x[2] === 2) {
+    out += 'final ';
+  }
+  switch (x[0]) {
+  case 0:
+    out += 'public ';
+    break;
+  case 1:
+    out += 'protected ';
+    break;
+  case 2:
+    out += 'private ';
+    break;
+  }
+  if (x[1] === 1) {
+    out += 'static ';
+  }
+  return out;
+};
