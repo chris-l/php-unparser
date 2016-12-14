f2111721c2b93af85025b0d1dad068aa621ab7e8
Christopher Luna <dev@christopher-luna.com>
2016-12-13 19:36:19 -0600

    Fix identation problems

--- node_translators/const.js
+++ node_translators/const.js
@@ -7,16 +7,14 @@
 module.exports = function (node, indent) {
   var codegen, decl = [], that = this;
   codegen = this.process.bind(this);
-  node[1].forEach(function(item, index) {
+  node[1].forEach(function (item, index) {
     decl.push(
-      (
-        index === 0 ?
-        indent + 'const ' :
-        that.nl + indent + '      '
-      )
-      + item[0]  // constant name
-      + that.ws + '=' + that.ws +
-      codegen(item[1], indent) // constant value
+      (index === 0 ?
+            indent + 'const ' :
+            that.nl + indent + '      ')
+        + item[0]  // constant name
+        + that.ws + '=' + that.ws +
+        codegen(item[1], indent) // constant value
     );
   });
   return decl.join(',' + this.ws);
