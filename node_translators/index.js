763735aad85e05d1f6b29892d5a88666dac82758
Ioan CHIRIAC <i.chiriac@yahoo.com>
2016-12-14 01:26:16 +0100

    add interfaces

--- node_translators/index.js
+++ node_translators/index.js
@@ -53,6 +53,7 @@ CodeGen.prototype.for = require("./for.js");
 CodeGen.prototype.function = require("./function.js");
 CodeGen.prototype.global = require("./global.js");
 CodeGen.prototype.if = require("./if.js");
+CodeGen.prototype.interface = require("./interface.js");
 CodeGen.prototype.link = require("./link.js");
 CodeGen.prototype.list = require("./list.js");
 CodeGen.prototype.lookup = require("./lookup.js");
--- node_translators/interface.js
+++ node_translators/interface.js
@@ -6,30 +6,21 @@ var addKeywords = require('./helper/keywords');
 module.exports = function (node, indent) {
   var codegen, str, that, sections;
   codegen = this.process.bind(this);
-  sections = node[5];
+  sections = node[4];
   that = this;
-  str = '';
 
   // Start
-  if (node[2] === 1) {
-    str += 'abstract ';
-  } else if (node[2] === -1) {
-    str += 'final ';
-  }
-
-  str += 'class ' + node[1];
+  str = node[2] === -1 ? 'final ' : '';
+  str += 'interface ' + node[1];
 
   if (node[3]) {
-    str += ' extends ' + node[3].join('\\');
-  }
-
-  if (node[4]) {
-    str += ' implements ' + node[4].map(function (x) { return x.join('\\'); }).join(',' + that.ws);
+    str += ' implements ' + node[3].map(function (x) {
+      return x.join('\\');
+    }).join(',' + that.ws);
   }
 
   // begin curly brace
   str += this.nl + indent + '{' + this.nl;
-
 
   /**
    * Prepare constants
@@ -47,29 +38,6 @@ module.exports = function (node, indent) {
       that.ws + codegen(prop[0][1], indent) + ';';
   }).join(this.nl) + this.nl;
 
-
-  /**
-   * Prepare properties
-   */
-  str += sections.properties.map(function (prop) {
-    var out = indent + that.indent;
-
-    // handle doc decoration
-    if (prop[0] === 'doc') {
-      out += prop[1] + that.nl + indent + that.indent;
-      prop = prop[2];
-    }
-
-    out += addKeywords(prop[2]);
-    out += prop[0];
-
-    if (prop[1]) {
-      out += that.ws + '=' + that.ws + codegen(prop[1], indent + that.indent);
-    }
-    return out + ';';
-  }).join(this.nl) + this.nl;
-
-
   /**
    * Prepare methods
    */
@@ -82,15 +50,8 @@ module.exports = function (node, indent) {
       method = method[2];
     }
 
-    // It lacks body. Is an abstract method.
-    if (method.length === 7) {
-      out += addKeywords(method[6]);
-      out += codegen(method.slice(0, 6), indent + that.indent, { notClosure : true });
-      return out;
-    }
-
-    out += addKeywords(method[7]);
-    out += codegen(method, indent + that.indent, { notClosure : true });
+    out += addKeywords(method[6]);
+    out += codegen(method.slice(0, 6), indent + that.indent, { notClosure : true });
     return out;
   }).join(this.nl + this.nl) + this.nl;
 
