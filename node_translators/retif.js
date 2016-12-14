361cf3190eb93bd65bee504bb7503d62bc62526c
Ioan CHIRIAC <i.chiriac@yahoo.com>
2016-12-14 01:43:35 +0100

    fix short if

--- node_translators/retif.js
+++ node_translators/retif.js
@@ -5,17 +5,18 @@ module.exports = function (node, indent) {
   var codegen, left, right;
 
   codegen = this.process.bind(this);
-  left = codegen(node[2], indent);
-  if (node[2][0] === 'retif') {
-    left = '(' + left + ')';
+  if (node[2]) {
+    left = codegen(node[2], indent);
+    if (node[2][0] === 'retif') {
+      left = '(' + left + ')';
+    }
   }
   right = codegen(node[3], indent);
   if (node[3][0] === 'retif') {
     right = '(' + right + ')';
   }
 
-  return codegen(node[1], indent) + this.ws + '?' + this.ws + left + this.ws + ':' + this.ws + right;
+  return codegen(node[1], indent) + this.ws + '?' +
+    (left ? this.ws + left + this.ws : '') + ':' + this.ws + right
+  ;
 };
-
-
-
