c429145e417b5a1a208368664e813ca6c565f897
Ioan CHIRIAC <i.chiriac@yahoo.com>
2016-12-15 11:42:06 +0100

    handle static declarations

--- node_translators/static.js
+++ node_translators/static.js
@@ -2,11 +2,11 @@
 'use strict';
 
 module.exports = function (node, indent) {
-  var codegen = this.process.bind(this),
-    prop = Array.isArray(node[3]) ? codegen(node[3], indent) : node[3];
+  var codegen = this.process.bind(this);
   if (node[1] === 'get') {
+    var prop = Array.isArray(node[3]) ? codegen(node[3], indent) : node[3];
     return codegen(node[2], indent) + '::' + prop;
+  } else if (node[1] === 'declare') {
+    return 'static ' + node[2].map(function (x) { return x[0] + ' = ' + codegen(x[1], indent); }).join(',' + this.ws);
   }
 };
-
-
