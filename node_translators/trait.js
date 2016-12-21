c96ae8e268b6998b29fb88783d6246dd9ac8b2ab
Ioan CHIRIAC <i.chiriac@yahoo.com>
2016-12-21 12:41:56 +0100

    update traits to 0.2

--- node_translators/trait.js
+++ node_translators/trait.js
@@ -4,10 +4,9 @@ var doBody = require('./helper/body');
 var addKeywords = require('./helper/keywords');
 
 module.exports = function (node, indent) {
-  var codegen, str, that, sections;
+  var codegen, str;
   codegen = this.process.bind(this);
-  sections = node[4];
-  that = this;
+
   str = 'trait ' + node[1];
 
   if (node[2]) {
@@ -15,82 +14,14 @@ module.exports = function (node, indent) {
   }
 
   if (node[3]) {
-    str += ' implements ' + node[3].map(function (x) { return x.join('\\'); }).join(',' + that.ws);
+    str += ' implements ' + node[3].map(function (x) { return x.join('\\'); }).join(',' + this.ws);
   }
 
   // begin curly brace
   str += this.nl + indent + '{' + this.nl;
 
-
-  /**
-   * Prepare constants
-   */
-  str += sections.constants.map(function (prop) {
-    var out = indent + that.indent;
-
-    // handle doc decoration
-    if (prop[0] === 'doc' || prop[0] === 'comment') {
-      out += prop[1] + that.nl + indent + that.indent;
-      prop = prop[2];
-    }
-    return out + 'const ' + prop[0] + that.ws + '=' +
-      that.ws + codegen(prop[1], indent) + ';';
-  }).join(this.nl) + this.nl;
-
-
-  /**
-   * Prepare properties
-   */
-  str += sections.properties.map(function (prop) {
-    var out = indent + that.indent;
-
-    // handle doc decoration
-    if (prop[0] === 'doc' || prop[0] === 'comment') {
-      out += prop[1] + that.nl + indent + that.indent;
-      prop = prop[2];
-    }
-
-    out += addKeywords(prop[2]);
-    out += prop[0];
-
-    if (prop[1]) {
-      out += that.ws + '=' + that.ws + codegen(prop[1], indent + that.indent);
-    }
-    return out + ';';
-  }).join(this.nl) + this.nl;
-
-
-  /**
-   * Prepare methods
-   */
-  str += '\n' + sections.methods.map(function (method) {
-    var out = indent + that.indent;
-
-    // handle doc decoration
-    if (method[0] === 'doc' || method[0] === 'comment') {
-      out += method[1] + that.nl + indent + that.indent;
-      method = method[2];
-    }
-
-    // It lacks body. Is an abstract method.
-    if (method.length === 7) {
-      out += addKeywords(method[6]);
-      out += codegen(method.slice(0, 6), indent + that.indent);
-      return out;
-    }
-
-    if (method[0] === 'comment' && method[2][0] === 'function') {
-      out += codegen(method, indent + that.indent);
-      out += that.nl + indent + that.indent;
-      out += addKeywords(method[2][7]);
-      out += codegen(method[2], indent + that.indent);
-      return out;
-    }
-
-    out += addKeywords(method[7]);
-    out += codegen(method, indent + that.indent);
-    return out;
-  }).join(this.nl + this.nl) + this.nl;
+  // trait body
+  str += doBody(codegen, indent, this.indent, this.nl, node[4]);
 
   // end curly brace
   str += indent + '}\n';
