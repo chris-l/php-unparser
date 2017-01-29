ac2897388fa46a124f26961b43cdd2b029b9f9b8
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-01-29 15:37:33 +0100

    remove old unused node (byref is now a boolean property of nodes

--- node_translators/byref.js
+++
@@ -1,7 +0,0 @@
-/*jslint node: true, indent: 2 */
-'use strict';
-
-module.exports = function (node, indent) {
-  var codegen = this.process.bind(this);
-  return '& ' + codegen(node[1], indent);
-};
