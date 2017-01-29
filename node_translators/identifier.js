502ad0e9257566f38a5b11c1e17e2ce58a33f1f6
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-01-29 15:37:01 +0100

    add new identifier node

---
+++ node_translators/identifier.js
@@ -0,0 +1,10 @@
+/*jslint node: true, indent: 2 */
+'use strict';
+
+module.exports = function (node) {
+  if (node.resolution === 'rn') {
+    return 'namespace\\' + node.name;
+  } else {
+    return node.name
+  }
+};
