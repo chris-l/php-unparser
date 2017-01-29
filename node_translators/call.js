87b7f21edacfc579e21bff51e68a8e1058b663b4
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-01-29 15:38:05 +0100

    migrate the call statement

--- node_translators/call.js
+++ node_translators/call.js
@@ -1,28 +1,9 @@
 /*jslint node: true, indent: 2 */
 'use strict';
 
+var params = require('./helper/parameters');
+
 module.exports = function (node, indent) {
-  var codegen, useArgs = [], space, args, raw;
-
-  codegen = this.process.bind(this);
-
-  function processElement(indent) {
-    return function (arg) {
-      return codegen(arg, indent);
-    };
-  }
-  if (node[2] && node[2].length > 0) {
-    useArgs = node[2].map(processElement(indent));
-  }
-  raw = useArgs.join();
-  if ((raw.indexOf("\n") > -1 && raw.substr(0, raw.indexOf("\n")).length > 80) || (raw.indexOf("\n") === -1 && raw.length > 80)) {
-    useArgs = node[2].map(processElement(indent + this.indent));
-    space = this.nl + indent + this.indent;
-    args = space + useArgs.join(',' + space) + this.nl + indent;
-  } else {
-    args = useArgs.join(',' + this.ws);
-  }
-
-  return codegen(node[1], indent) + '(' + args + ')';
+  return codegen(node.what, indent) +
+    '(' + params(node.arguments, indent, this) + ')';
 };
-
