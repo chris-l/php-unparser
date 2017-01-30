03616214ad9a98a93cae7934d16beed4396ee07e
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-01-30 23:39:17 +0100

    wip / migrate namespace node

--- node_translators/namespace.js
+++ node_translators/namespace.js
@@ -6,9 +6,9 @@ module.exports = function (node, indent) {
   var str, codegen;
 
 
-  str = 'namespace ' + node[1].join('\\') + this.ws + '\n{\n\n';
+  str = 'namespace ' + node.name.name + this.ws + '\n{\n\n';
   codegen = this.process.bind(this);
-  str += body(codegen, indent, this.indent, this.nl, node[2]);
+  str += body(codegen, indent, this.indent, this.nl, node.children);
   str += '}';
 
   return str;
