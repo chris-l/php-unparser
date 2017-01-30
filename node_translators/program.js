b27e2c3e2d0a5b663c4b6557ebfb0f690188a121
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-01-30 23:39:02 +0100

    migrate program node

--- node_translators/program.js
+++ node_translators/program.js
@@ -4,10 +4,13 @@ var body = require('./helper/body');
 
 module.exports = function (node) {
   var codegen = this.process.bind(this);
-
-  if (!this.forceNamespaceBrackets && node[1].length === 1 && node[1][0][0] === 'namespace') {
-    return 'namespace ' + node[1][0][1].join('\\') + ';\n\n' +
-      body(codegen, '', this.indent, this.nl, node[1][0][2], true);
+  if (
+    !this.forceNamespaceBrackets &&
+      node.children.length === 1 &&
+      node.children[0].kind === 'namespace'
+  ) {
+    return 'namespace ' + node.children[0].name.name + ';\n\n' +
+      body(codegen, '', this.indent, this.nl, node.children[0].children, true);
   }
-  return body(codegen, '', this.indent, this.nl, node[1], true);
+  return body(codegen, '', this.indent, this.nl, node.children, true);
 };
