bbf238df2b4673f71f2d83b405b5dda4574ac8de
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-01-30 23:39:33 +0100

    migrate call nodes

--- node_translators/call.js
+++ node_translators/call.js
@@ -4,6 +4,7 @@
 var params = require('./helper/parameters');
 
 module.exports = function (node, indent) {
+  var codegen = this.process.bind(this);
   return codegen(node.what, indent) +
     '(' + params(node.arguments, indent, this) + ')';
 };
