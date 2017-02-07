9139b9d41e8ab4f7afa1247231488343a1e5feda
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-02-07 22:46:18 +0100

    migrate encapsed

--- node_translators/encapsed.js
+++ node_translators/encapsed.js
@@ -4,6 +4,7 @@
 module.exports = function (node, indent, opt) {
   var body = '';
   var codegen = this.process.bind(this);
+
   node.value.forEach(function(item) {
     if (item.kind === 'string') {
       body += item.value;
@@ -11,10 +12,16 @@ module.exports = function (node, indent, opt) {
       body += '{' + codegen(item, indent) + '}';
     }
   });
+
   if (node.type === 'heredoc') {
     return '<<<' + node.label + this.nl + body + node.label;
+  } else if (node.type === 'nowdoc') {
+    return '<<<\'' + node.label + '\'' + this.nl + body + node.label;
   }
-  console.log(node);
-  throw new Error('@todo');
-  return JSON.stringify(node[1]);
+
+  if (node.isDoubleQuote) {
+    return '"' + body + '"';
+  }
+
+  return '\'' + body + '\'';
 };
