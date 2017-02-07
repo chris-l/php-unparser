7d7d67f7dbb9de0150fef3e11d0b3fc29bc6d24c
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-02-07 22:45:48 +0100

    migrate cast

--- node_translators/cast.js
+++ node_translators/cast.js
@@ -3,28 +3,6 @@
 
 module.exports = function (node, indent) {
   var codegen, str;
-
   codegen = this.process.bind(this);
-  switch (node[1]) {
-  case 'boolean':
-    str = '(bool)';
-    break;
-  case 'int':
-    str = '(int)';
-    break;
-  case 'double':
-    str = '(float)';
-    break;
-  case 'string':
-    str = '(string)';
-    break;
-  case 'array':
-    str = '(array)';
-    break;
-  case 'object':
-    str = '(object)';
-    break;
-  }
-  return str + codegen(node[2], indent);
+  return '(' + node.type + ')' + codegen(node.what, indent);
 };
-
