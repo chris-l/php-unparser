1b3905aeffb7ed5392af09a2df090446b73099d7
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-02-17 23:18:10 +0100

    fix empty script

--- node_translators/program.js
+++ node_translators/program.js
@@ -3,6 +3,11 @@
 var body = require('./helper/body');
 
 module.exports = function (node) {
+
+  if (!node.children || node.children.length === 0) {
+    return '';
+  }
+
   var codegen = this.process.bind(this), str = '<?php' + this.nl;
   if (node.children[0].kind === 'inline') {
     str = node.children[0].value + str;
