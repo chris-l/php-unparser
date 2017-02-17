843b593e267f406194151dee999a9e0b7851f6d0
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-02-17 11:12:07 +0100

    migrate traits output

--- node_translators/trait.js
+++ node_translators/trait.js
@@ -1,27 +1,27 @@
 /*jslint node: true, indent: 2 */
 'use strict';
 var doBody = require('./helper/body');
-var addKeywords = require('./helper/keywords');
+var identifier = require('./helper/identifier');
 
 module.exports = function (node, indent) {
   var codegen, str;
   codegen = this.process.bind(this);
 
-  str = 'trait ' + node[1];
+  str = 'trait ' + node.name;
 
-  if (node[2]) {
-    str += ' extends ' + node[2].join('\\');
+  if (node.extends) {
+    str += ' extends ' + identifier(node.extends);
   }
 
-  if (node[3]) {
-    str += ' implements ' + node[3].map(function (x) { return x.join('\\'); }).join(',' + this.ws);
+  if (node.implements) {
+    str += ' implements ' + node.implements.map(identifier).join(',' + this.ws);
   }
 
   // begin curly brace
   str += this.nl + indent + '{' + this.nl;
 
   // trait body
-  str += doBody(codegen, indent, this.indent, this.nl, node[4]);
+  str += doBody(codegen, indent, this.indent, this.nl, node.body);
 
   // end curly brace
   str += indent + '}\n';
