ec71d689fdad1aa2fbdd79afc22ea10a0b4b897a
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-02-17 13:51:35 +0100

    implement do/while statement

--- node_translators/do.js
+++ node_translators/do.js
@@ -1,12 +1,14 @@
 /*jslint node: true, indent: 2 */
 'use strict';
+var doBody = require('./helper/body');
 
 module.exports = function (node, indent) {
   var codegen, str;
   codegen = this.process.bind(this);
   str = 'do' + this.ws + '{' + this.nl;
-  str += require('./helper/body')(codegen, indent, this.indent, this.nl, node[2]);
-  str += indent + '}' + this.ws + 'while' + this.ws + '(' + codegen(node[1], indent) + ')';
+  str += doBody(
+    codegen, indent, this.indent, this.nl, node.body.children
+  );
+  str += indent + '}' + this.ws + 'while' + this.ws + '(' + codegen(node.test, indent) + ')';
   return str;
 };
-
