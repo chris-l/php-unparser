98e40ffdc5179707ee974ba4b3926214b817021b
Christopher Luna <dev@christopher-luna.com>
2017-07-08 03:50:34 -0500

    The parenthesis from "new" were missing

--- node_translators/new.js
+++ node_translators/new.js
@@ -6,8 +6,10 @@ module.exports = function (node, indent) {
   var codegen, str;
   codegen = this.process.bind(this);
   str = codegen(node.what, indent);
+  str += '(';
   if (node.arguments && node.arguments.length > 0) {
     str += params(node.arguments, indent, this);
   }
+  str += ')';
   return 'new ' + str;
 };
