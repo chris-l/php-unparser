ea27422083ed051243ad3727bfe2b23d7b366b8b
Christopher Luna <dev@christopher-luna.com>
2018-03-07 21:05:11 -0600

    Fix usegroup; it should use "," instead of ";"

--- node_translators/usegroup.js
+++ node_translators/usegroup.js
@@ -5,25 +5,26 @@
  * Usage declaration
  */
 module.exports = function (node, indent) {
-  var str = 'use' + this.ws, items = [], glue;
+  var str = 'use' + this.ws, items, glue;
   if (node.type) {
     str += node.type + this.ws;
   }
-  node.items.forEach(function (item) {
+
+  items = (node.items || []).map(function (item) {
     var useItem = item.name;
     if (item.alias) {
       useItem += ' as ' + item.alias;
     }
-    useItem += ';';
-    items.push(useItem);
+    return useItem;
   });
+
   if (node.items.length > 1) {
     glue = this.nl +  indent + this.indent;
     str += node.name + this.ws + '{' + glue;
-    str += items.join(glue) + this.nl;
+    str += items.join(',' + glue) + this.nl;
     str += indent + '};' + this.nl;
   } else {
-    str += items[0] + this.nl;
+    str += items[0] + ';' + this.nl;
   }
   return str;
 };
