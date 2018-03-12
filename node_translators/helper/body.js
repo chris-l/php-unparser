72a8785681dd7687d0a0ddbccb06a165d7b128e3
Christopher Luna <dev@christopher-luna.com>
2018-03-12 02:58:03 -0600

    helper/body.js: Converted map to for
    That was changed to allow to change if the delimiter is used or not

--- node_translators/helper/body.js
+++ node_translators/helper/body.js
@@ -10,7 +10,7 @@ var noSemiColons = [
 
 module.exports = function (codegen, currentIndent, body, isProgram, dontIncreaseIndent) {
 
-  var str, indentation, delimiter, that = this;
+  var str = '', expr, i, indentation, delimiter, that = this, line, next;
 
   // Set the rows delimiter
   delimiter = that.options.collapseEmptyLines ? '' : '\n';
@@ -27,37 +27,35 @@ module.exports = function (codegen, currentIndent, body, isProgram, dontIncrease
     body = [body];
   }
 
-  // Map body values
-  str = body.map(function (expr, index) {
-    var line, next;
+  for (i = 0; i < body.length; i += 1) {
+    expr = body[i];
 
     // Return empty string
-    if (expr === null) {
-      return '';
+    if (expr !== null) {
+
+      if (expr.kind === 'label') {
+        line = codegen(expr, indentation);
+      } else {
+        line = indentation + codegen(expr, indentation);
+      }
+
+      // This expressions don't require semicolons
+      if (noSemiColons.indexOf(expr.kind) === -1) {
+        line += ';';
+      }
+
+      // Check if the next expression is a comment that should be
+      // on the same line as this expression
+      next = body[i + 1] || {};
+      if (next.kind === 'doc' && next.loc && expr.loc && next.loc.start.line === expr.loc.start.line) {
+        line += that.ws + codegen(next, '').trim();
+        next.alreadyParsed = true; // prevent to parse again the comment
+      }
+
+      str += line + that.nl + delimiter;
     }
 
-    if (expr.kind === 'label') {
-      line = codegen(expr, indentation);
-    } else {
-      line = indentation + codegen(expr, indentation);
-    }
-
-    // This expressions don't require semicolons
-    if (noSemiColons.indexOf(expr.kind) === -1) {
-      line += ';';
-    }
-
-    // Check if the next expression is a comment that should be
-    // on the same line as this expression
-    next = body[index + 1] || {};
-    if (next.kind === 'doc' && next.loc && expr.loc && next.loc.start.line === expr.loc.start.line) {
-      line += that.ws + codegen(next, '').trim();
-      next.alreadyParsed = true; // prevent to parse again the comment
-    }
-
-    return line + that.nl;
-
-  }).join(delimiter);
+  }
 
   // Return the generated string
   return str;
